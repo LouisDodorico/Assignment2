6a60f5d72b1835fd5d1cb6196b5be17ab43ce382
LouisDodorico <106502342+LouisDodorico@users.noreply.github.com>
2025-04-28 14:43:17 +0930

    changed if statement as sr needs to buffer out of order packets not drop them and created offset

--- gbn.c
+++ gbn.c
@@ -212,7 +212,8 @@ void B_input(struct pkt packet)
   int i;
 
   /* if not corrupted and received packet is in order */
-  if  ( (!IsCorrupted(packet))  && (packet.seqnum == expectedseqnum) ) {
+  if  (!IsCorrupted(packet)) {
+    int offset = (packet.seqnum - expectedseqnum + SEQSPACE) % SEQSPACE;
     if (TRACE > 0)
       printf("----B: packet %d is correctly received, send ACK!\n",packet.seqnum);
     packets_received++;
